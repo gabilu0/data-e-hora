0f5fa260e2926582f48706c01998735de63c6742
Gabriel Santos <gabilubastos@gmail.com>
2023-07-14 16:48:22 -0300

    instanciando objeto Project

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,10 @@ package application;
 
 import entities.Product;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -25,9 +28,25 @@ public class Program {
 
         boolean success = new File(sourceFolderStr + "\\out").mkdir();
 
-        
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
 
-        System.out.println("Folder create: " + success);
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+            String itemCsv = br.readLine();
+            while(itemCsv != null) {
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                Double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
 
         sc.close();
